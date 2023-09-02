a38fb203fd042fbe98f7099b68e2023c9514799e
Daniel Lee <danlee@hey.com>
2023-09-01 20:27:49 -0600

    adding browser html config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+        ],
+    },
+    plugins: [
+        // uses template and injects bundled js files into specified html
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        }),
+    ]
 }
