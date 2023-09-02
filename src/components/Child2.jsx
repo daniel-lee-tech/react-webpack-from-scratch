a67ef258802032fe6c9fc102f136e210dfd4bbb5
Daniel Lee <danlee@hey.com>
2023-09-02 10:22:11 -0600

    [RWS-8] Adding webpack config for css and css modules

---
+++ src/components/Child2.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Child2.module.css';
+
+export function Child2() {
+    return (
+        <div>
+            <p className={styles.p}>Child 2</p>
+        </div>
+    )
+}
