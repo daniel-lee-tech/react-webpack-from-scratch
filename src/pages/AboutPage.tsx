d24bbe66494ebf56a5a17b3b8560e69a3e189e8d
Dan Lee <87612696+daniel-lee-tech@users.noreply.github.com>
2023-09-03 20:25:20 -0600

    [RWS-15] Adding react router and cleaning up non relevant code (#8)
    tsconfig.json has needed config changes so inline imports of css modules and images do not have red squiggly errors.

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function AboutPage() {
+    return (<p>About page</p>)
+}
---
+++ src/pages/ImagePresenterPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {ImagePresenter} from "../components/ImagePresenter";
+
+export function ImagePresenterPage() {
+    return (<div>
+        <ImagePresenter defaultShow={true} />
+    </div>)
+}
